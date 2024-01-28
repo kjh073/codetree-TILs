c1c0fb07d1785ffaeb50c641cfbde2a681192e21
Joohee KIm <82260747+kjh073@users.noreply.github.com>
2024-01-29 04:58:24 +0900

    Title: 숫자의 순차적 이동, Time: 238ms, Memory: 26MB, Status: Passed - CodeTree

--- 240129/숫자의 순차적 이동/sequential-movement-of-numbers.py
+++ 240129/숫자의 순차적 이동/sequential-movement-of-numbers.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-
+# print(arr)
 dx = [-1, -1, -1, 0, 1, 1, 1, 0]
 dy = [-1, 0, 1, 1, 1, 0, -1, -1]
 
@@ -32,9 +32,12 @@ def switch(x, y):
     arr[nx][ny] = arr[x][y]
     arr[x][y] = tmp
 
-for num in range(1, n * n + 1):
-    x, y = find_pos(num)
-    switch(x, y)
+
+for i in range(m):
+	for num in range(1, n * n + 1):
+		x, y = find_pos(num)
+		switch(x, y)
+    
 
 for r in range(n):
     for c in range(n):
