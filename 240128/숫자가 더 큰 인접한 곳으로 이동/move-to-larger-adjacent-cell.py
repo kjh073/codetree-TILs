e49e1dbc42e5405af3d5d8e72379d7528c8c97c4
Joohee KIm <82260747+kjh073@users.noreply.github.com>
2024-01-28 20:50:49 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240128/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240128/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,5 +1,7 @@
-n, x, y = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+# n, x, y = map(int, input().split())
+# arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [[1, 6, 7], [2, 5, 8], [3, 4, 9]]
+n, x, y = 3, 1, 1
 move_num = []
 x, y = x - 1, y - 1
 move_num.append(arr[x][y])
@@ -11,17 +13,22 @@ dy = [0, 0, -1, 1]
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
-move_cnt = 1
-
 def move(x, y):
     max_num = arr[x][y]
-    for dir_num in range(4):
-        nx = x + dx[dir_num]
-        ny = y + dy[dir_num]
-        if in_range(nx, ny) and arr[nx][ny] > max_num:
-            x, y = nx, ny
-            max_num = arr[nx][ny]
-            move_num.append(arr[nx][ny])
+    while True:
+        for dir_num in range(4):
+            nx = x + dx[dir_num]
+            ny = y + dy[dir_num]
+            pre_max_x, pre_max_y = x, y
+            if in_range(nx, ny) and arr[nx][ny] > max_num:
+                x, y = nx, ny
+                max_x, max_y = x, y
+                max_num = arr[x][y]
+                move_num.append(arr[x][y])
+                break
+        # print(max_x, max_y, pre_max_x, pre_max_y)
+        if max_x == pre_max_x and pre_max_y == y:
+            break
             
 move(x, y)
 
