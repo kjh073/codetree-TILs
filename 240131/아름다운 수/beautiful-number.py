4ac444194ecb92ea986eb70201aa4f11c8265eba
Joohee KIm <82260747+kjh073@users.noreply.github.com>
2024-01-31 02:39:10 +0900

    Title: 아름다운 수, Time: 277ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240131/아름다운 수/beautiful-number.py
+++ 240131/아름다운 수/beautiful-number.py
@@ -14,7 +14,7 @@ def is_beautiful(num):
                 return False
             elif num[i - 1] == max_num:
                 max_num = 1
-    if max_num != num[n - 1]:
+    if max_num % num[n - 1] != 0:
         return False
     return True         
             
@@ -22,18 +22,13 @@ def is_beautiful(num):
 def choose(pos):
     global cnt
     if pos == n + 1:
+        # print_answer()
         if is_beautiful(num):
+            # print_answer()
             cnt += 1
-            # print(num)
-        # print(is_beautiful(num))
-        # print(answer)
-        # print()
-        # print(len(answer))
         return
     
     for i in range(1, n + 1):
-        # if i != 1 and n - len(num) < i:
-        #     return
         num.append(i)
         choose(pos + 1)
         num.pop()
