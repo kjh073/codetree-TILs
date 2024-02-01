f0d693b44a337cd6f26851bd1576e94a5c80cf85
Joohee KIm <82260747+kjh073@users.noreply.github.com>
2024-02-02 01:03:28 +0900

    Title: 그래프 탐색, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240202/그래프 탐색/graph-traversal.py
+++ 240202/그래프 탐색/graph-traversal.py
@@ -2,11 +2,11 @@ n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(m)]
 graph = [[] for _ in range(n + 1)]
 visited = [False for _ in range(n + 1)]
+cnt =  0
 
 def dfs(vertex):
     for curr_v in graph[vertex]:
-        if not visited[vertex]:
-            print(curr_v)
+        if not visited[curr_v]:
             visited[vertex] = True
             dfs(curr_v)
 
@@ -14,8 +14,11 @@ for start, end in arr:
     graph[start].append(end)
     graph[end].append(start)
 
-# root_vertex = 1
-# visited[root_vertex] = True
-# print(root_vertex)
-# dfs(root_vertex)
-print(len(graph[1]))
+root_vertex = 1
+visited[root_vertex] = True
+dfs(root_vertex)
+
+for visit in visited:
+    if visit == True:
+        cnt +=1 
+print(cnt)
