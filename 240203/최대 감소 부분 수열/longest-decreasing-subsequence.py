ec47b71c0d68f8895834a4c61cd25785446887b9
Joohee KIm <82260747+kjh073@users.noreply.github.com>
2024-02-03 04:33:50 +0900

    Title: 최대 감소 부분 수열, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240203/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -0,0 +1,29 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+n = int(input())
+dp = [0] * n
+arr = list(map(int, input().split()))
+
+def initialize():
+    for i in range(n):
+        dp[i] = 1
+    dp[0] = 1
+
+initialize()
+    
+for i in range(1, n):
+    for j in range(i):
+        if dp[j] == INT_MAX:
+            continue
+        if arr[j] > arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+
+            
+result = 0
+for i in range(n):
+    result = max(result, dp[i])
+# print(dp)
+print(result)
