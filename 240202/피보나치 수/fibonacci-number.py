2f58b5276cee1b4bc3d30ea409f408d6fc6552e6
Joohee KIm <82260747+kjh073@users.noreply.github.com>
2024-02-02 21:53:34 +0900

    Title: 피보나치 수, Time: 170ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/피보나치 수/fibonacci-number.py
+++ 240202/피보나치 수/fibonacci-number.py
@@ -1,8 +1,10 @@
 n = int(input())
 fibo = [-1] * (n + 1)
-fibo[1], fibo[2] = 1, 1
 
-for i in range(3, n + 1):
-    fibo[i] = fibo[i - 1] + fibo[i - 2]
+for i in range(1, n + 1):
+    if i == 1 or i == 2:
+        fibo[i] = 1
+    else:
+        fibo[i] = fibo[i - 1] + fibo[i - 2]
     
 print(fibo[n])
